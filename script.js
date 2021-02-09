b5402a7faf7511e57f61b45020e6bd5a6058c9ec
rafael <rafaelbarroslimaa@gmail.com>
2021-02-08 21:15:55 -0300

    Add script.js

--- script.js
+++ script.js
@@ -12,3 +12,36 @@ const modal = {
     .remove('active')
   }
 }
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '23/01/2021',
+  },
+  {
+    id: 2,
+    description: 'Website',
+    amount: 500000,
+    date: '23/01/2021',
+  },
+  {
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '23/01/2021',
+  },
+]
+
+const transaction = {
+  incomes() {
+    // Somar as entradas
+  },
+  expenses() {
+    // Somar as saídas
+  },
+  total() {
+    // Entradas - saídas
+  }
+}
